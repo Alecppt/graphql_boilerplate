ed3564c333d701327cda034a12e56096a687af73
Alec Chen <goodneighboralec@gmail.com>
2020-04-14 04:44:08 -0700

    getting rid of extra db connection call

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,23 +1,21 @@
 import { IResolvers } from 'graphql-tools';
 import * as bcrypt from 'bcrypt';
 import { User } from './entity/User';
-import { createTypeormConnection } from './util/createTypeormConnection';
 
 export const resolvers: IResolvers = {
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) =>
-      `Hello ${name || 'World'}`
+      `Hello ${name || 'World'}`,
   },
   Mutation: {
     register: async (
       _,
       { email, password }: GQL.IRegisterOnMutationArguments
     ) => {
-      await createTypeormConnection();
       const hashPassword = await bcrypt.hash(password, 10);
       const user = await User.create({ email, password: hashPassword });
       await user.save();
       return true;
-    }
-  }
+    },
+  },
 };
